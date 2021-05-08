2f30c86bd184d49ae83d54bab9dd8313aa937719
Ritesh Jangra <riteshk2874@gmail.com>
2021-05-08 16:29:55 +0530

    final push

--- index.js
+++ index.js
@@ -88,4 +88,4 @@ app.get('/:name', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log('app listening on port 3000!'));
+app.listen(3000 || process.env.PORT, () => console.log('app listening on port 3000!'));
